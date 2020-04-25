26ea05a67786802422b1d1c0b7fab8b218b6c060
Michael Kuijpers <michaelkuijpers89@gmail.com>
2020-04-25 11:42:35 +0200

    ook ongesorteerde lijst methode toegvoegd

--- src/sequential/Main.java
+++ src/sequential/Main.java
@@ -15,17 +15,25 @@ public class Main {
 
     // Class variabels
     private static FileRead file;
-    private static Scanner input;
+    private static Scanner year;
+    private static Scanner askOnsorted;
+    private static List<Result> onsorted;
 
     public static void main(String[] args) {
 
         // Vraag om het jaartal waar je t/m wil filteren
-        System.out.print("Enter max year: ");
-        input = new Scanner(System.in);
-        String inputYear = input.nextLine();
+        System.out.print("Enter max year that you want to sort: ");
+        year = new Scanner(System.in);
+        String inputYear = year.nextLine();
+        // Vraag of de ongesorte lijst ook getoond moet worden
+        System.out.print("Do you also want to see the onsorted list first j/n? ");
+        askOnsorted = new Scanner(System.in);
+        String answerOnSorted = askOnsorted.nextLine();
         // lees de lijst uit op basis van de input jaar
         file = new FileRead();
         List<Result> results = file.createDataFromFile(inputYear);
+        onsorted = file.createDataFromFile(inputYear);
+        onSortedPrint(answerOnSorted, inputYear);
 
         // Merge sort de lijst
         MergeSort test = new MergeSort((ArrayList<Result>) results);
@@ -33,6 +41,23 @@ public class Main {
         // Toon de resultaten
         test.toonSort();
 
+    }
+
+    /**
+     * Maakt een print wanneer er een behoefte is tot een onsorted print
+     * @param answerOnSorted
+     */
+    private static void onSortedPrint(String answerOnSorted, String inputYear) {
+        System.out.println("Onsorted list:");
+        if(answerOnSorted.equals("j")) {
+            for(int i = 0; i < onsorted.size(); i++) {
+                Result result = onsorted.get(i);
+                System.out.println(result.getYear() + ": " + result.getTemperature());
+            }
+        } else {
+            return;
+        }
+        System.out.println("==============================\n");
     }
 
 }
